8c799989ce45624e8593bf22825ae2ee3672ce73
Ravenholdt <joppe4899@hotmail.com>
2020-12-11 16:19:39 +0000

    Proper deltaVcalculation

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -11,9 +11,12 @@ float calculateDeltaV(float mass, float radius, float atmosphere)
 {
     float period = calculateOrbitalPeriod(mass, radius);
     float velocity = calculateOrbitalVelocity(period, radius);
-    return velocity * 1.2 + atmosphere * 0.2;
+    return velocity * 1.1 + velocity * atmosphere * 0.1;
 }
 
 float calculateGravity(float mass, float radius){ return (G * mass) / pow(radius, 2); }
 float calculateOrbitalPeriod(float mass, float distance){ return 2.0 * 3.14 * sqrt( pow(distance, 3) / (mass * G) ); }
 float calculateOrbitalVelocity(float orbitalPeriod, float distance) { return (2 * PI * distance) / orbitalPeriod; }
+
+
+// Basera pris på volym, sätt upp handelspunkter som noder.

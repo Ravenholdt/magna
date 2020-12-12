16fe7de3599a3537da9577aae07373312cc5c6fc
Ravenholdt <joppe4899@hotmail.com>
2020-12-12 22:16:59 +0000

    Simple solar system

--- src/generateSol.cpp
+++ src/generateSol.cpp
@@ -17,6 +17,7 @@ void generateTerra(int sun)
 
     m = galaxy.celestials[p].newChild(3.85e8, 7.3459e22, 1.7374e6, CelestialType::moon); // Moon
 		galaxy.celestials[m].name = "Luna";
+        galaxy.celestials[m].habitability = 0.2;
 	c = galaxy.celestials[m].newColony(m); // Lunar Base
 		galaxy.colonies[c].population = 10000;
 }
@@ -28,36 +29,80 @@ int generateSol()
 
 	int p, m;
 	p = galaxy.celestials[sun].newChild(6.76e10, 3.301e23, 2.44e6, CelestialType::planet); // Mercury
-		galaxy.celestials[p].name = "Mercery";
-		galaxy.celestials[p].atmosphere = 0;
+		galaxy.celestials[p].name = "Mercury";
+        galaxy.celestials[p].habitability = 0.1;
 
 	p = galaxy.celestials[sun].newChild(1.078e11, 4.867e24, 6.05e6, CelestialType::planet); // Venus
 		galaxy.celestials[p].name = "Venus";
+        galaxy.celestials[p].habitability = 0.001;
 
     generateTerra(sun);
 
 	p = galaxy.celestials[sun].newChild(2.207e11, 6.417e23, 2.39e6, CelestialType::planet); // Mars
 		galaxy.celestials[p].name = "Mars";
+        galaxy.celestials[p].habitability = 0.2;
     {
         m = galaxy.celestials[p].newChild(9.4e6, 1.072e16, 1.108e4, CelestialType::moon); // Phobos
 			galaxy.celestials[m].name = "Phobos";
-			galaxy.celestials[p].atmosphere = 0;
+            galaxy.celestials[m].habitability = 0.1;
         m = galaxy.celestials[p].newChild(2.3e7, 1.5e15, 6.2e3, CelestialType::moon); // Deimos
 			galaxy.celestials[m].name = "Deimos";
-			galaxy.celestials[p].atmosphere = 0;
+            galaxy.celestials[m].habitability = 0.1;
     }
 
 	p = galaxy.celestials[sun].newChild(7.636e11, 1.898e27, 6.995e7, CelestialType::planet); // Jupiter
 		galaxy.celestials[p].name = "Jupiter";
+        galaxy.celestials[p].habitability = 0;
+	{
+		m = galaxy.celestials[p].newChild(4.217e8, 8.931938e22, 1.8216e6, CelestialType::moon); // Io
+			galaxy.celestials[m].name = "Io";
+            galaxy.celestials[m].habitability = 0.1;
+		m = galaxy.celestials[p].newChild(6.709e8, 4.799844e22, 1.5608e6, CelestialType::moon); // Europa
+			galaxy.celestials[m].name = "Europa";
+            galaxy.celestials[m].habitability = 0.2;
+		m = galaxy.celestials[p].newChild(1.0716e9, 1.4819e23, 2.6341e6, CelestialType::moon); // Ganymede
+			galaxy.celestials[m].name = "Ganymede";
+            galaxy.celestials[m].habitability = 0.1;
+		m = galaxy.celestials[p].newChild(1.8827e9, 1.0759e23, 2.41e6, CelestialType::moon); // Callisto
+			galaxy.celestials[m].name = "Callisto";
+            galaxy.celestials[m].habitability = 0.1;
+	}
 
 	p = galaxy.celestials[sun].newChild(1.495e12, 5.683e26, 5.83e7, CelestialType::planet); // Saturn
 		galaxy.celestials[p].name = "Saturn";
+        galaxy.celestials[p].habitability = 0;
+	{
+        m = galaxy.celestials[p].newChild(1.85539e8, 3.7493e19, 1.982e5, CelestialType::moon); // Minmas
+			galaxy.celestials[m].name = "Minmas";
+            galaxy.celestials[m].habitability = 0.1;
+        m = galaxy.celestials[p].newChild(2.37948e8, 1.08022e20, 2.521e5, CelestialType::moon); // Enceladus
+			galaxy.celestials[m].name = "Enceladus";
+            galaxy.celestials[m].habitability = 0.1;
+        m = galaxy.celestials[p].newChild(2.94619e8, 6.17449e20, 5.311e5, CelestialType::moon); // Tethys
+			galaxy.celestials[m].name = "Tethys";
+            galaxy.celestials[m].habitability = 0.1;
+        m = galaxy.celestials[p].newChild(3.77396e8, 1.095452e21, 1.1228e6, CelestialType::moon); // Dione
+			galaxy.celestials[m].name = "Dione";
+            galaxy.celestials[m].habitability = 0.1;
+        m = galaxy.celestials[p].newChild(5.27108e8, 2.306518e21, 7.638e5, CelestialType::moon); // Rhea
+			galaxy.celestials[m].name = "Rhea";
+            galaxy.celestials[m].habitability = 0.1;
+        m = galaxy.celestials[p].newChild(1.221870e9, 1.3452e23, 2.57473e6, CelestialType::moon); // Titan
+			galaxy.celestials[m].name = "Titan";
+            galaxy.celestials[m].habitability = 0.01;
+			galaxy.celestials[p].atmosphere = 1.45;
+        m = galaxy.celestials[p].newChild(3.560820e9, 1.805635e21, 1.4690e6, CelestialType::moon); // Iapetus
+			galaxy.celestials[m].name = "Iapetus";
+            galaxy.celestials[m].habitability = 0.1;
+	}
 
 	p = galaxy.celestials[sun].newChild(2.958e12, 8.681e25, 2.536e7, CelestialType::planet); // Uranus
 		galaxy.celestials[p].name = "Uranus";
+        galaxy.celestials[p].habitability = 0;
 
 	p = galaxy.celestials[sun].newChild(4.477e12, 1.024e26, 2.46e7, CelestialType::planet); // Nepture
 		galaxy.celestials[p].name = "Neptune";
+        galaxy.celestials[p].habitability = 0;
 
 	return sol;
 }
